5a9343d0482cc89e5feb781df63c7b446f6a5738
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-10-23 17:55:35 +0700

    try using 'from django.contrib.auth.models import User' instead

--- wordwise/models/user_data.py
+++ wordwise/models/user_data.py
@@ -1,7 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
-
-User = get_user_model()
 
 
 class UserData(models.Model):
