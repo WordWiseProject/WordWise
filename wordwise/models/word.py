d26bfa36760e5801e09fd299dc41c36dcaf24f8d
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-10-29 22:41:07 +0700

    remove primary key from vocab, and let django create a primary key

--- wordwise/models/word.py
+++ wordwise/models/word.py
@@ -13,5 +13,5 @@ class Word(models.Model):
     :type collection: Collection
     """
 
-    vocab = models.CharField(max_length=255, primary_key=True)
+    vocab = models.CharField(max_length=255)
     collection = models.ManyToManyField(Collection)
