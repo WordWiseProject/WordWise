5f24743ad33a55645e7dd1d3ab25faa4c0df29b0
Peanutt47 <pnchalee@gmail.com>
2023-10-29 23:14:45 +0700

    Refs #13: fix url in get_word_from_type_of function & add url in url.py

--- wordwise/urls.py
+++ wordwise/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('flashcard.html', views.flashcard_view, name='flashcard'),
     path('jeopardy.html', views.jeopardy_view, name='jeopardy'),
+    path('flashcard.html/<type_of>', views.flashcard_view, name='flashcard'),
 ]
--- wordwise/views.py
+++ wordwise/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 
 from .models import Definition, TypeOf, Word
 
+import random
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 env = environ.Env()
 
@@ -36,26 +38,32 @@ def get_word(word: str):
                     defi.type_of.add(get_type_of)
                 except TypeOf.DoesNotExist:
                     get_type_of = TypeOf(type_of=type_of)
+                    get_type_of.save()
                     defi.type_of.add(get_type_of)
-
     return word
 
-def get_word_from_type_of(request):
-    url = f"https://wordsapiv1.p.mashape.com/words/{type}/hasTypes"
+
+def get_word_from_type_of(type):
+    url = f"https://wordsapiv1.p.rapidapi.com/words/{type}/hasTypes"
     response = requests.get(url, headers=HEADERS)
     type_json = response.json()
     for results in type_json["hasTypes"]:
         get_word(word=results)
-    word_list = list(Definition.objects.filter(type_of=type))
-    return random.choice(word_list)
+    word_list = list(Definition.objects.filter(type_of__type_of=type))
+    ran_word_list = random.sample(word_list, 10)
+    return ran_word_list
 
 
 def home(request):
-    return render(request, "wordwise/index.html")
+    context = {'type_of': ['business', 'sport', 'technology', 'study']}
+    return render(request, "wordwise/index.html", context)
 
 
-def flashcard_view(request):
-    return render(request, "wordwise/flashcard.html")
+def flashcard_view(request, type_of):
+    word_list = get_word_from_type_of(type_of)
+    context = {'word_list': word_list}
+    return render(request, "wordwise/flashcard.html", context)
+
 
 def jeopardy_view(request):
     return render(request, "wordwise/jeopardy.html")
