69d279873d1434af51a323cc9b3fbff81985b81b
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-04 17:07:57 +0700

    Refs #16: Implement censoring string that will be used in the fill-in-the-blank with testing

--- wordwise/models/example.py
+++ wordwise/models/example.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 
 from .definition import Definition
@@ -15,3 +17,24 @@ class Example(models.Model):
 
     example = models.CharField(max_length=255)
     example_of = models.ForeignKey(Definition, on_delete=models.CASCADE)
+
+    def censor(self) -> str:
+        """Censor the root word."""
+        if self.example_of.word.vocab[-1] == "e":
+            censored_sentence = re.sub(
+                re.escape(self.example_of.word.vocab[:-1]),
+                "_" * len(self.example_of.word.vocab[:-1]),
+                self.example,
+                flags=re.IGNORECASE,
+            )
+        else:
+            censored_sentence = re.sub(
+                re.escape(self.example_of.word.vocab),
+                "_" * len(self.example_of.word.vocab),
+                self.example,
+                flags=re.IGNORECASE,
+            )
+        return censored_sentence
+
+    def __str__(self) -> str:
+        return self.example
---
+++ wordwise/tests/test_example.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from wordwise.models import Definition, Example, Word
+
+
+class TestAPI(TestCase):
+    def test_censor_of_example(self):
+        word = Word(vocab="test")
+        defi = Definition(word=word, definition="test", part_of_speech="test")
+        exam = Example(example_of=defi, example="We are testing")
+        self.assertEqual(exam.censor(), "We are ____ing")
+
+        exam = Example(example_of=defi, example="We are done tested")
+        self.assertEqual(exam.censor(), "We are done ____ed")
+
+        exam = Example(example_of=defi, example="Testing")
+        self.assertEqual(exam.censor(), "____ing")
+
+        word2 = Word(vocab="example")
+        defi2 = Definition(word=word2, definition="test", part_of_speech="test")
+        exam = Example(
+            example_of=defi2, example="In the classroom, the teacher was exampling a new concept to the students."
+        )
+        self.assertEqual(exam.censor(), "In the classroom, the teacher was ______ing a new concept to the students.")
