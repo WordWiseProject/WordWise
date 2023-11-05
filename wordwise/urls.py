e2775d0aa4cb73befbb6a2e4e3de34292ede54cd
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-05 11:03:09 +0700

    delete unuse path

--- wordwise/urls.py
+++ wordwise/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("flashcard.html", views.flashcard_view, name="flashcard"),
     path("jeopardy.html", views.jeopardy_view, name="jeopardy"),
     path("flashcard.html/<type_of>", views.flashcard_view, name="flashcard"),
-    path("fill", views.FillInTheBlank.as_view(), name="fill_in"),
 ]
