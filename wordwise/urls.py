511a08edd294321c8450ed35b8c413658529fb82
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-05 11:00:42 +0700

    delete .idea folder, and reformat some files

--- wordwise/urls.py
+++ wordwise/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('flashcard.html', views.flashcard_view, name='flashcard'),
-    path('jeopardy.html', views.jeopardy_view, name='jeopardy'),
-    path('flashcard.html/<type_of>', views.flashcard_view, name='flashcard'),
+    path("", views.home, name="index"),
+    path("flashcard.html", views.flashcard_view, name="flashcard"),
+    path("jeopardy.html", views.jeopardy_view, name="jeopardy"),
+    path("flashcard.html/<type_of>", views.flashcard_view, name="flashcard"),
+    path("fill", views.FillInTheBlank.as_view(), name="fill_in"),
 ]
