78d357419416ea93a064b9f53d8bc2e231e878dd
Tantikon Phasanphaengsi <tantikon.p@ku.th>
2023-11-06 15:23:18 +0700

    closed #16: complete fill-in-the-blank mode

--- wordwise/forms/test_form.py
+++ wordwise/forms/test_form.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class TestFrom(forms.Form):
-    answer = forms.CharField(label="Your Answer", max_length=100)
+    answer = forms.CharField(
+        max_length=100,
+        label="Answer",
+        label_suffix=" :",
+        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs mx-auto my-4 input-sm"}),
+    )
