697b4278b5e1f1bcac4dec99880ab012fb83e2c7
Tantikon Phasanphaengsi <tantikon.p@ku.th>
2023-11-12 04:09:32 +0700

    update table

--- wordwise/forms/collection_form.py
+++ wordwise/forms/collection_form.py
@@ -8,12 +8,12 @@ from wordwise.models import Collection
 # input input-bordered input-md w-full max-w-xs mt-2
 class CollectionForm(forms.Form):
     name = forms.CharField(
-        label="Name",
+        label="Name ",
         max_length=100,
         widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs mx-auto my-4 input-sm"}),
     )
     description = forms.CharField(
-        label="Description",
+        label="Description ",
         max_length=100,
         widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs mx-auto my-4 input-sm"}),
     )
