60be5461dd233160be59131a5269adece6379a77
Peanutt47 <pnchalee@gmail.com>
2023-11-13 00:50:20 +0700

    add more card on index page

--- wordwise/views.py
+++ wordwise/views.py
@@ -67,7 +67,7 @@ def get_list_word_from_type_of(type):
 class Home(View):
     def get(self, request):
         type_list = TypeOf.objects.all()
-        random_type_list = random.sample(list(type_list), 7)
+        random_type_list = random.sample(list(type_list), 8)
         context = {"type_of": random_type_list}
         print(random_type_list)
         return render(request, "wordwise/index.html", context)
