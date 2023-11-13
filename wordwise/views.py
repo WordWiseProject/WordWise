1901c6da6ffc45bd3dc990b5c1ede8611c8397f1
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-13 22:22:02 +0700

    add redirect to correct url from fill in the blank mode

--- wordwise/views.py
+++ wordwise/views.py
@@ -119,11 +119,17 @@ def check_fill_in_the_blank_answer(request):
 
 class FillInTheBlank(View):
     def get(self, request):
+        if request.session.get("current_deck") != 0:
+            request.session["current_deck"] = 0
         word_list = sorted(Definition.objects.filter(example__isnull=False), key=lambda x: random.random())
         p = Paginator(word_list, 1)
         page = request.GET.get("page")
         defi = p.get_page(page)
-        return render(request, "wordwise/fill_in_blank.html", {"defi": defi, "form": FillInTheBlankForm})
+        return render(
+            request,
+            "wordwise/fill_in_blank.html",
+            {"defi": defi, "form": FillInTheBlankForm, "current_deck": request.session.get("current_deck")},
+        )
 
     def post(self, request):
         return check_fill_in_the_blank_answer(request)
@@ -143,7 +149,11 @@ class FillInTheBlankDeck(View):
         p = Paginator(word_list, 1)
         page = request.GET.get("page")
         defi = p.get_page(page)
-        return render(request, "wordwise/fill_in_blank.html", {"defi": defi, "form": FillInTheBlankForm})
+        return render(
+            request,
+            "wordwise/fill_in_blank.html",
+            {"defi": defi, "form": FillInTheBlankForm, "current_deck": request.session.get("current_deck")},
+        )
 
     def post(self, request):
         return check_fill_in_the_blank_answer(request)
@@ -233,6 +243,49 @@ class DeckTestMode(View):
         random.shuffle(random_definition_list)
         form = TestForm(definition=random_definition_list, current=current_defi.id)
         return render(request, "wordwise/test_mode.html", {"defi": defi, "current_deck": pk, "form": form})
+
+    def post(self, request):
+        answer = request.POST.get("definition")
+        correct_defi = request.POST.get("current")
+        next_page = int(request.POST.get("next_page_number"))
+        has_next = bool(strtobool(request.POST.get("has_next")))
+        current_deck = request.session.get("current_deck")
+        current_defi = Definition.objects.get(id=correct_defi)
+        contexts = {
+            "next_page": next_page,
+            "has_next": has_next,
+            "correct_defi": current_defi,
+            "current_deck": current_deck,
+        }
+        if not has_next:
+            request.session.pop("random_seed")
+        if answer == correct_defi:
+            return render(request, "wordwise/test_pass.html", context=contexts)
+        return render(request, "wordwise/test_fail.html", context=contexts)
+
+
+class QuickTestMode(View):
+    def get(self, request):
+        if not request.session.get("random_seed", False):
+            request.session["random_seed"] = random.randint(1, 10000)
+        if request.session.get("current_deck") != 0:
+            request.session["current_deck"] = 0
+        word_list = list(Definition.objects.all())
+        random.seed(request.session.get("random_seed"))
+        random_definition_list = list(Definition.objects.all().order_by("?")[:3])
+        random.shuffle(word_list)
+        p = Paginator(word_list, 1)
+        page = request.GET.get("page")
+        defi = p.get_page(page)
+        current_defi = defi.object_list[0]
+        random_definition_list.append(current_defi)
+        random.shuffle(random_definition_list)
+        form = TestForm(definition=random_definition_list, current=current_defi.id)
+        return render(
+            request,
+            "wordwise/test_mode.html",
+            {"defi": defi, "current_deck": request.session.get("current_deck"), "form": form},
+        )
 
     def post(self, request):
         answer = request.POST.get("definition")
