4ffdf543d4bd88340692a23a35f2f72a2471a7bf
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-26 01:20:31 +0700

    Refs #49: update random favorite list appearance

--- wordwise/forms/random_fav_from.py
+++ wordwise/forms/random_fav_from.py
@@ -3,8 +3,21 @@ from django import forms
 from wordwise.models import Definition
 
 
+class CustomModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        # Define how you want each choice's label to be displayed
+        return f"{obj.word.vocab} - {obj.definition}"
+
+
 class RandomFavoriteForm(forms.Form):
     def __init__(self, *args, **kwargs):
         fav_list = kwargs.pop("fav_list")
         super().__init__(*args, **kwargs)
-        self.fields["definition"] = forms.ModelChoiceField(queryset=fav_list, widget=forms.RadioSelect())
+
+        def label_from_instance(obj):
+            return f"{obj.word} - {obj.definition}"
+
+        self.fields["definition"] = CustomModelChoiceField(
+            queryset=fav_list,
+            widget=forms.RadioSelect(),
+        )
