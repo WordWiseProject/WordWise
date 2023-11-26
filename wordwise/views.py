cbaaf841066a525e87ee2c6447a3431ec4385393
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-26 17:11:31 +0700

    always lock deck when create a new deck

--- wordwise/views.py
+++ wordwise/views.py
@@ -225,7 +225,7 @@ class DeckCreateView(View):
         if form.is_valid():
             name = form.cleaned_data["name"]
             desc = form.cleaned_data["description"]
-            collection = WordDeck(name=name, description=desc, user=request.user)
+            collection = WordDeck(name=name, description=desc, user=request.user, private=False)
             collection.save()
             return redirect("wordwise:deck_index")
         return redirect("wordwise:deck_index")
